85d2c9494c742895295d2d2ba942c77dff8722f2
Pieta9221 <101437355+Pieta9221@users.noreply.github.com>
2022-05-03 12:47:47 +0100

    Update 9-strcpy.c

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -13,9 +13,9 @@ char *_strcpy(char *dest, char *src, int n)
 	int i;
 
 	i = 0;
-	while (i < n && src [i] != '\0')
+	while (i < n && src[i] != '\0')
 	{
-		dest[i] = src [i];
+		dest[i] = src[i];
 		i++;
 	}
 
