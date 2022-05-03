0b96598596c614e4314765c22d28d54c435cb52f
Pieta9221 <101437355+Pieta9221@users.noreply.github.com>
2022-05-03 12:11:56 +0100

    Update 9-strcpy.c

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,23 +1,24 @@
+#include<stdio.h>
 #include "main.h"
-#include <stdio.h>
 /**
- * _strcpy - copies the string pointed to by src,
- * including the terminating null byte, to the
- * buffer pointed to by dest.
- * @dest: destination.
- * @src: source.
- * Return: the pointer to dest.
+ * _strcpy - update
+ * @dest: value
+ * @src: value
+ * Return:not
  */
 char *_strcpy(char *dest, char *src)
 {
-	int count = 0;
+	int l = 0;
+	int x = 0;
 
-	while (count >= 0)
+	while (*(src + l) != '\0')
 	{
-		*(dest + count) = *(src + count);
-		if (*(src + count) == '\0')
-			break;
-		count++;
+		l++;
 	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
 	return (dest);
 }
